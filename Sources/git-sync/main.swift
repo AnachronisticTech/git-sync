637937ac515326e43c4aa1490602b47bf08f8a2b
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2019-12-17 11:35:23 +0200

    Added options list and logout()

--- Sources/git-sync/main.swift
+++ Sources/git-sync/main.swift
@@ -6,6 +6,24 @@ let config = TokenConfiguration(token)
 let client = Octokit(config)
 var me: User? = nil
 
+func main() {
+    login()
+    var validOption = false
+    repeat {
+        validOption = true
+        switch listOptions() {
+//        case "1": listRepos()
+//        case "2": listStarredRepos()
+//        case "3": generateGitSync()
+//        case "4": cloneFromGitSync()
+        case "0": logout()
+        default :
+            validOption = false
+            print("That is not a valid task")
+        }
+    } while !validOption
+}
+
 func login() {
     var authenticated: Bool? = nil
     client.me { response in
@@ -25,4 +43,22 @@ func login() {
     if !authenticated! {
         exit(1)
     }
+}
+
+func listOptions() -> String {
+    print("\nPlease select a task:")
+    print("  1. List your repositories")
+    print("  2. List your starred repositories")
+    print("  3. Generate git-sync file")
+    print("  4. Clone from git-sync file")
+    print("  0. Exit")
+    return readLine()!
+}
+
+func logout() {
+    exit(0)
+}
+
+while true {
+    main()
 }
