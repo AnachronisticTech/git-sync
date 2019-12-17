3becf8506268e910d8b66d06e1aaaf73f9fd72f1
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2019-12-17 11:33:33 +0200

    Added login() to retrieve GitHub details by token

--- Sources/git-sync/main.swift
+++ Sources/git-sync/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+import Foundation
+import OctoKit
+
+let token = ""
+let config = TokenConfiguration(token)
+let client = Octokit(config)
+var me: User? = nil
+
+func login() {
+    var authenticated: Bool? = nil
+    client.me { response in
+        switch response {
+        case .success(let user):
+            print("Welcome to git-sync, \(user.login!)")
+            me = user
+            authenticated = true
+        case .failure(let error):
+            print(error)
+            authenticated = false
+        }
+    }
+    repeat {
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
+    } while authenticated == nil
+    if !authenticated! {
+        exit(1)
+    }
+}
